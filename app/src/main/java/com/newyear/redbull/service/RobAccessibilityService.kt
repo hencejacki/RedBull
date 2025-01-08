69bea5cccc03207af78e4226413498ba1ecd6eb7
hencejacki <hencejacki@gmail.com>
2025-01-08 23:30:25 +0800

    add:accessibility service

---
+++ app/src/main/java/com/newyear/redbull/service/RobAccessibilityService.kt
@@ -0,0 +1,14 @@
+package com.newyear.redbull.service
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class RobAccessibilityService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
